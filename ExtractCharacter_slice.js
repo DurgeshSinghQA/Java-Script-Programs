e2caa62ab05c54d393e76ab4facfc8d7faed0cee
Durgesh Kumar <durgeshkumarsdet@gmail.com>
2026-02-15 14:02:43 +0530

    Extract characters using slice method in java script

---
+++ ExtractCharacter_slice.js
@@ -0,0 +1,9 @@
+let str = "Software Development Endineer In Test";
+
+let last4Chars = str.slice(-4); // extracts the last 4 characters of the string
+
+console.log("The last 4 characters of the string are: " + last4Chars);
+
+let first4Chars = str.slice(0, 4); // extracts the first 4 characters of the string
+
+console.log("The first 4 characters of the string are: " + first4Chars);
