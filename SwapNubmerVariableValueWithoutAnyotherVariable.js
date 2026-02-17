00cc6a5f80bb321ee5be6877c4b0cd7c5b18705a
Durgesh Kumar <durgeshkumarsdet@gmail.com>
2026-02-17 14:13:15 +0530

    Swap Nubmer Variable Value Without Any other Variable java script program

---
+++ SwapNubmerVariableValueWithoutAnyotherVariable.js
@@ -0,0 +1,10 @@
+let num1  = 4;
+let num2 = 5;
+
+console.log("Before swapping: num1 = " + num1 + ", num2 = " + num2);
+
+num1 = num1 + num2
+num2 = num1 - num2
+num1 = num1 - num2  
+
+console.log("After swapping: num1 = " + num1 + ", num2 = " + num2);
