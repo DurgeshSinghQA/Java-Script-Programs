e1a7c41d55074ad725709af8ac092c34dd8e178a
Durgesh Kumar <durgeshkumarsdet@gmail.com>
2026-02-17 15:07:53 +0530

    Center Aligned Star Pyramid java script program

---
+++ CenterAlignedStarPyramid.js
@@ -0,0 +1,13 @@
+for(let i=0; i<5; i++) {
+
+    for(let j=5; j>i; j--) {
+        process.stdout.write(" ");
+    }
+    for(let k=0; k<=i; k++) {
+        process.stdout.write("*");
+    }
+    for(let l=0; l<i; l++) {
+        process.stdout.write("*");
+    }
+    console.log();
+}
