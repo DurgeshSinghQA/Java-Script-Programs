6fee99450b9b8fd6b1643f0ed800023f8ef0e06a
Durgesh Kumar <durgeshkumarsdet@gmail.com>
2026-02-22 09:53:03 +0530

    Refactored code pattern 1a2b3c

--- abbcccPatternFroma1b2c3.js
+++ abbcccPatternFroma1b2c3.js
@@ -1,25 +1,35 @@
-/* String str = "a1b2c3";
-Print below pattern
-a
-bb
-ccc
-*/
+let str = '1a2b3c';
+//let str = 'a1b2c3';
 
-let str = "a1b2c3";
-
-if(!isNumber(str.charAt(0))) 
+function isNumber(char)
 {
-    for(let i=0; i<str.length; i+=2) 
-    {
-        let count = parseInt(str.charAt(i+1));
-        for(let j=0; j<count; j++) {
-            process.stdout.write(str.charAt(i));
-        }
-        process.stdout.write('\n'); 
-    }
-   
+	return /^[0-9]$/.test(char);
+
 }
 
-function isNumber(char) {
-    return /^[0-9]$/.test(char);    
+if(!isNumber(str.charAt(0)))
+{
+	for(let i=0; i<str.length; i+=2)
+	{
+		let len = parseInt(str.charAt(i+1));
+		for(let j=0; j<len; j++)
+		{
+			process.stdout.write(str.charAt(i));
+		}
+		process.stdout.write('\n');
+	}
+}
+
+else if(isNumber(str.charAt(0)))
+{
+	for(let i = 0; i < str.length; i+=2)
+	{
+		let len = parseInt(str.charAt(i));
+		for(let j = 0; j < len; j++)
+		{
+			process.stdout.write(str.charAt(i+1));
+		}
+
+		process.stdout.write('\n');
+	}
 }
